71f0f61f6cac2fcf421169f5b19e8f7cb9c4e955
Nick Simmons <nicksimmons53@gmail.com>
2024-08-25 15:24:24 -0500

    Navbar fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,32 +4,38 @@ import { Inter, Raleway } from "next/font/google";
 import "./globals.css";
 import {Button, Navbar, NavbarContent, NavbarItem, NextUIProvider} from "@nextui-org/react";
 import {finishAuth} from "@/app/auth/route";
-import {redirect} from "next/navigation";
+import {redirect, usePathname} from "next/navigation";
+import {useRouter} from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 const raleway = Raleway({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
+  const path = usePathname();
   const signOut = async() => {
     finishAuth();
   }
+
+  console.log(path);
 
   return (
     <html lang="en">
       <body className={raleway.className}>
         <NextUIProvider>
-          <Navbar isBordered>
-            <NavbarContent justify={"start"}>
-              <NavbarItem>
-                <h1>BNA Automations</h1>
-              </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify={"end"}>
-              <NavbarItem>
-                <Button onClick={signOut} color={"danger"}>Sign Out</Button>
-              </NavbarItem>
-            </NavbarContent>
-          </Navbar>
+          {!path.includes("/login") && (
+            <Navbar isBordered>
+              <NavbarContent justify={"start"}>
+                <NavbarItem>
+                  <h1>BNA Automations</h1>
+                </NavbarItem>
+              </NavbarContent>
+              <NavbarContent justify={"end"}>
+                <NavbarItem>
+                  <Button onClick={signOut} color={"danger"}>Sign Out</Button>
+                </NavbarItem>
+              </NavbarContent>
+            </Navbar>
+          )}
           {children}
         </NextUIProvider>
       </body>
